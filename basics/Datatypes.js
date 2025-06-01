5dfdeebdd1b011981f5b956f40c3a66bdbb4a5b7
YashJaybhaye <168146670+YashJaybhaye@users.noreply.github.com>
2025-06-01 10:45:00 +0530

    Create Datatypes.js
    Learn Datatypes in js

---
+++ basics/Datatypes.js
@@ -0,0 +1,13 @@
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+// undefined has it's own datatype that is 'undefined' it self 
+//BUT Null is an 'object' 
